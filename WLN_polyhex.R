214944db3a6162ba670123dde3719a388262ffb7
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-04-15 08:42:20 -0700

    Create WLN_polyhex.R

---
+++ WLN_polyhex.R
@@ -0,0 +1,9 @@
+#The following represents a hexagonal coordinate system used in Wiswesser Line Notation (WLN) 
+#for enumerating polyhexes using WLN.
+#
+WLN_polyhex<-function(h){
+#h represents the number of poly(hexes)
+  h<-as.numeric(h)
+  
+
+}
