04715f3bdc4447f259d71955e32a968ee5f5fd6a
Matthew S. MacLennan <msmaclennan@gmail.com>
2016-05-25 21:17:02 -0700

    vector-based and faster

--- code2cm.R
+++ code2cm.R
@@ -1,24 +1,30 @@
-#c11 is the output from alkenum()
-#This code transforms alkenum() output into connectivity matrices in order to apply isomorphism test. 
-code2cm<-function(c11,wordlength){
-require(igraph);
-c12<-lapply(strsplit(as.character(c11),""),as.numeric)
+code2cm2<-function(c11,wordlength){
+c12<-lapply(strsplit(as.character(c11)," "),as.numeric)
 list1<-list()
 for(j in 1:length(c12)){
 a<-c();b<-c();c<-c();
 	for(i in 1:length(c12[[j]])){
+
+
 		a<-c(a,rep(i,c12[[j]][i]))
-#print(a)
+print(a)
 		c<-rep(0,c12[[j]][i])
-#print(c)
-		if (length(c)==0 && i<wordlength) {b[max(gregexpr("(?<!0)0",paste0(b,collapse=""),perl=T)[[1]])]<-(i+1)} else {if(i==wordlength) {next} else {c[1]<-i+1}}
-#print(c)
+print(c)
+		if (length(c)==0) {
+			if(i<wordlength){
+#ISSUE: index derived from this is not 
+#b[max(gregexpr("(?<!0) 0 ",paste0(b,collapse=" "),perl=T)[[1]])+1]<-(i+1)} else {break}} else {c[1]<-i+1}
+					b[max(na.omit((seq_along(b)[b!=0]+1)[b[seq_along(b)[b!=0]+1]==0]))]<-(i+1)} else {break}} else {c[1]<-i+1}
+
+
+print(c)
 
 		b<-c(b,c)
-#print(b)
+print(b)
+
 	}
 		list1[[j]]<-cbind(a,b)
  }
-
 list1
 }
+
